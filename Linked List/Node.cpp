0c4b8986cedce3e2f03f80a82236d0e05b3d4796
Ayush Gupta <721ayush@gmail.com>
2020-08-24 08:27:06 +0530

    linked list program

---
+++ Linked List/Node.cpp
@@ -0,0 +1,55 @@
+/*
+ *   Copyright (c) 2020 
+ *   All rights reserved.
+ */
+#include <iostream>
+using namespace std;
+
+class Node
+{
+public:
+    int data;
+    Node *next;
+
+    Node(int data)
+    {
+        this->data = data;
+        next = NULL;
+    }
+};
+void print(Node *head)
+{
+    while (head != NULL)
+    {
+        cout << head->data << " ";
+        head = head->next;
+    }
+}
+int main()
+{
+    //  statically
+    Node n1(1);
+    Node *head = &n1;
+    Node n2(2);
+    Node n3(3);
+    Node n4(4);
+    Node n5(5);
+
+    n1.next = &n2;
+    n2.next = &n3;
+    n3.next = &n4;
+    n4.next = &n5;
+
+    print(head);
+
+    // n1.next = &n2;
+    // cout << Head->data << endl;
+    // cout << n1.data << " " << n2.data << endl;
+
+    // // dynamically
+    // Node *n3 = new Node(10);
+    // Node *head = n3;
+    // Node *n4 = new Node(20);
+
+    // n3->next = n4;
+}
