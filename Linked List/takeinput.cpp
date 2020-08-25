2ad78edc1189c187d4c7a169dbb197eae3e3748e
Ayush Gupta <721ayush@gmail.com>
2020-08-25 06:57:09 +0530

    takeinput in linkedlist

--- Linked List/takeinput.cpp
+++ Linked List/takeinput.cpp
@@ -18,4 +18,52 @@ public:
     }
 };
 
+Node *takeInput()
+{
+    int data;
+    cin >> data;
+    Node *head = NULL;
+    while (data != -1)
+    {
+        Node *newNode = new Node(data);
+        if (head == NULL)
+        {
+            head = newNode;
+        }
+        else
+        {
+
+            Node *temp = head;
+            while (temp->next != NULL)
+            {
+                temp = temp->next;
+            }
+            temp->next = newNode;
+        }
+        cin >> data;
+    }
+    return head;
+}
+
+void print(Node *head)
+{
+    Node *temp = head;
+    while (temp != NULL)
+    {
+        cout << temp->data << " -> ";
+        temp = temp->next;
+    }
+    cout << endl;
+    // temp = head;
+    // while (temp != NULL)
+    // {
+    //     cout << temp->data << " -> ";
+    //     temp = temp->next;
+    // }
+}
+
 int main()
+{
+    Node *head = takeInput();
+    print(head);
+}
