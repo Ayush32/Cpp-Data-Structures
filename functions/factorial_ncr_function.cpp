6b257e80e62edb25403bc4290c064fbfbeca7e5b
Ayush Gupta <721ayush@gmail.com>
2020-09-02 21:17:44 +0530

    factorial program

---
+++ functions/factorial_ncr_function.cpp
@@ -0,0 +1,19 @@
+/*
+ *   Copyright (c) 2020 
+ *   All rights reserved.
+ */
+#include <iostream>
+using namespace std;
+
+int fact(int n)
+{
+    ;
+}
+
+int main()
+{
+    long long int n;
+    cin >> n;
+
+    return 0;
+}
