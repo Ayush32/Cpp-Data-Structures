c90068753caa2cc66043f69f1828444ee3d77a14
Ayush Gupta <721ayush@gmail.com>
2020-09-03 07:41:47 +0530

    factorial

--- functions/factorial_ncr_function.cpp
+++ functions/factorial_ncr_function.cpp
@@ -7,13 +7,23 @@ using namespace std;
 
 int fact(int n)
 {
-    ;
+    int ans = 1;
+    for (int i = 1; i <= n; i++)
+    {
+        ans = ans * i;
+    }
+    return ans;
+}
+int NCR(int n, int r)
+{
+    int ans = fact(n) / fact(n - r) * fact(r);
+    return ans;
 }
 
 int main()
 {
-    long long int n;
-    cin >> n;
+    cout << fact(5) << endl;
+    cout << NCR(5, 2) << endl;
 
     return 0;
 }
