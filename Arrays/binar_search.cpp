d681290c6a8e5b624f52b06bee6a0ea0102f18d6
Ayush Gupta <721ayush@gmail.com>
2020-09-03 18:37:52 +0530

    binary serach

---
+++ Arrays/binar_search.cpp
@@ -0,0 +1,49 @@
+/*
+ *   Copyright (c) 2020 
+ *   All rights reserved.
+ */
+#include <iostream>
+using namespace std;
+
+// s - start of yhe array
+// e -  end of the array
+
+int binary_search(int arr[], int n, int key)
+{
+    int s = 0;
+    int e = n - 1;
+    while (s <= e)
+    {
+        int mid = (s + e) / 2;
+
+        if (arr[mid] == key)
+        {
+            return mid;
+        }
+
+        if (arr[mid] > key)
+        {
+            e = mid - 1;
+        }
+        else
+        {
+            s = mid + 1;
+        }
+    }
+    return -1;
+}
+
+int main()
+{
+    int n;
+    int i;
+    int arr[1000];
+    cin >> n;
+
+    for (i = 0; i < n; i++)
+    {
+        cin >> arr[i];
+    }
+    int key = 0;
+    cin >> key;
+}
