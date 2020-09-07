0a7b754f70267781e77ebd3afefb830f2caf05f9
Ayush Gupta <721ayush@gmail.com>
2020-09-07 20:33:44 +0530

    matrix search complete

---
+++ 2d arrays/matrix_search.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int m, n, key;
+    cin >> m >> n;
+
+    int arr[m][n];
+    for (int row = 0; row < m; row++)
+    {
+        for (int col = 0; col < n; col++)
+        {
+            cin >> arr[row][col];
+        }
+    }
+    cin >> key;
+    int count = 0;
+    for (int row = 0; row < m; row++)
+    {
+        for (int col = 0; col < n; col++)
+        {
+            if (arr[row][col] == key)
+            {
+                count = 1;
+            }
+        }
+    }
+    if (count == 1)
+    {
+        cout << 1;
+    }
+    else
+    {
+        cout << 0;
+    }
+    return 0;
+}
