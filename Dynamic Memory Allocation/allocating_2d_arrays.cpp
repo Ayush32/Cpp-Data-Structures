20a3c24fa4e885c4739154f3918490e25eb34be2
Ayush Gupta <721ayush@gmail.com>
2020-09-09 08:32:26 +0530

    input complete

--- Dynamic Memory Allocation/allocating_2d_arrays.cpp
+++ Dynamic Memory Allocation/allocating_2d_arrays.cpp
@@ -2,3 +2,34 @@
  *   Copyright (c) 2020 
  *   All rights reserved.
  */
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+
+    int **arr;
+    int r, c;
+
+    cin >> r >> c;
+    // create am 2d array
+    arr = new int *[r];
+
+    for (int i = 0; i < r; i++)
+    {
+        arr[i] = new int[c];
+    }
+    int val = 1;
+    for (int i = 0; i < r; i++)
+    {
+        for (int j = 0; j < c; j++)
+        {
+            arr[i][j] = val;
+            val++;
+            cout << arr[i][j];
+        }
+        cout << endl;
+    }
+    return 0;
+}
