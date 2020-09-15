a96274859d8b920bdbbc241030e6caf7e0a82e5c
Ayush Gupta <721ayush@gmail.com>
2020-09-15 11:58:31 +0530

    bubble sort

---
+++ Recursion/bubleSOrt.cpp
@@ -0,0 +1,64 @@
+/*
+ *   Copyright (c) 2020 
+ *   All rights reserved.
+ */
+#include <iostream>
+using namespace std;
+
+void bubble_sort(int a[], int n)
+{
+    if (n == 1)
+    {
+        return;
+    }
+
+    for (int j = 0; j < n - 1; j++)
+    {
+        if (a[j] > a[j + 1])
+        {
+            swap(a[j], a[j + 1]);
+        }
+    }
+    bubble_sort(a, n - 1);
+}
+
+void bubble_sort_recursive(int a[], int j, int n)
+{
+    // base case
+    if (n == 1)
+    {
+        return;
+    }
+    // base case
+
+    if (j == n)
+    {
+        return;
+    }
+    // recursive case
+
+    if (a[j] > a[j + 1])
+    {
+        swap(a[j], a[j + 1]);
+    }
+    bubble_sort_recursive(a, j + 1, n - 1);
+}
+
+int main()
+{
+
+    int a[] = {5, 4, 3, 1, 2, 6, 7, 3, 6, 35, 67, 35, 3, 42, 53};
+    int n = sizeof(a) / sizeof(int);
+    bubble_sort(a, n);
+    for (int i = 0; i < n; i++)
+    {
+        cout << a[i] << " ";
+    }
+    cout << endl;
+    bubble_sort_recursive(a, 0, n);
+    for (int j = 0; j < n; j++)
+    {
+        cout << a[j] << " ";
+    }
+    return 0;
+}
--- Recursion/fast_power.cpp
+++ Recursion/fast_power.cpp
@@ -2,3 +2,29 @@
  *   Copyright (c) 2020 
  *   All rights reserved.
  */
+
+#include <iostream>
+using namespace std;
+
+int fast_power(int a, int n)
+{
+    if (n == 0)
+    {
+        return 1;
+    }
+    int smaller_ans = fast_power(a, n / 2);
+    smaller_ans *= smaller_ans;
+    if (n % 2 != 0)
+    {
+        return smaller_ans * a;
+    }
+    return smaller_ans;
+}
+
+int main()
+{
+    int a, n;
+    cin >> a >> n;
+    cout << fast_power(a, n);
+    return 0;
+}
