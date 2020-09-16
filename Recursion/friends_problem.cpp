47da45a09eef9e01f95a631a85fd90e937e3159b
Ayush Gupta <721ayush@gmail.com>
2020-09-16 17:20:30 +0530

    friends problem

--- Recursion/friends_problem.cpp
+++ Recursion/friends_problem.cpp
@@ -2,3 +2,29 @@
  *   Copyright (c) 2020 
  *   All rights reserved.
  */
+#include <iostream>
+using namespace std;
+
+int friends(int n)
+{
+    if (n == 0)
+    {
+        return 1;
+    }
+
+    if (n == 1)
+    {
+        return 1;
+    }
+
+    return friends(n - 1) + friends(n - 1) * friends(n - 2);
+}
+
+int main()
+{
+    int n;
+    cin >> n;
+    cout << friends(n);
+
+    return 0;
+}
