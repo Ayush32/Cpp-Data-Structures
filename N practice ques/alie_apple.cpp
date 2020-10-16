2e1007e489a44e66a88fb16497ed26fffb3015ae
Ayush Gupta <721ayush@gmail.com>
2020-10-16 08:17:02 +0530

    question

---
+++ N practice ques/alie_apple.cpp
@@ -0,0 +1,28 @@
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int minPerimeter(int n)
+{
+    int l = sqrt(n);
+    int sq = l * l;
+    if (sq == n)
+        return l * 4;
+    else
+    {
+        // Number of rows
+        long long int row = n / l;
+        long long int perimeter = 2 * (l + row);
+        if (n % l != 0)
+            perimeter += 2;
+        return perimeter;
+    }
+}
+
+// Driver code
+int main()
+{
+    int n = 1;
+    cout << minPerimeter(n);
+    return 0;
+}
---
+++ N practice ques/alie_apple.py
@@ -0,0 +1,6 @@
+apples = int(input())
+totalapples = 0;
+unit = 0;
+while (apples > totalapples):
+        totalapples += 12 * pow(unit, 2)
+print(unit * 8)
